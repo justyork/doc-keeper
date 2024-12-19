da72cee7681c459286cc6b30327856abc26083cf
york <yorkshp@gmail.com>
2024-12-19 08:35:51 +0200

    Add 'standard' and 'resource_type' filters to the array

--- app/routes/view.php
+++ app/routes/view.php
@@ -5,5 +5,7 @@ $controller = new ViewController();
 $filters = [
     'subject' => $_GET['subject'] ?? null,
     'subtopic' => $_GET['subtopic'] ?? null,
+    'standard' => $_GET['standard'] ?? null,
+    'resource_type' => $_GET['resource_type'] ?? null,
 ];
 $controller->render($filters);
