64646cfc6c5a7b39a7facc1a621f00bb80254a59
york <yorkshp@gmail.com>
2024-12-19 06:08:17 +0200

    Remove redundant assignment to $_SERVER array

--- app/helpers/Dotenv.php
+++ app/helpers/Dotenv.php
@@ -17,7 +17,6 @@ class Dotenv {
             $value = trim($value);
 
             if (!array_key_exists($name, $_SERVER) && !array_key_exists($name, $_ENV)) {
-                putenv(sprintf('%s=%s', $name, $value));
                 $_ENV[$name] = $value;
                 $_SERVER[$name] = $value;
             }
